309f9bcbde46845b9db3e64024696c7c9867fbd6
Andrii <artamonov06@gmail.com>
2020-05-08 00:02:24 +0200

    Implement profile picture attachment from gallery functionality and its editing on settings screen

--- app/src/main/java/com/artamonov/look4/UserProfileEditActivity.kt
+++ app/src/main/java/com/artamonov/look4/UserProfileEditActivity.kt
@@ -1,19 +1,30 @@
 package com.artamonov.look4
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.preference.PreferenceManager
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_user_profile_edit.*
 import kotlinx.android.synthetic.main.activity_welcome.*
 
 class UserProfileEditActivity : AppCompatActivity() {
 
+    var newImage: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile_edit)
 
-        populateData()
+        checkForPermissions()
 
         user_edit_submit_button.setOnClickListener {
             if (!fieldsAreValid()) {
@@ -24,18 +35,85 @@ class UserProfileEditActivity : AppCompatActivity() {
             val isSaved = editor
                 .putString(USER_NAME, user_edit_name.text.toString())
                 .putString(USER_PHONE_NUMBER, user_edit_phone_number.text.toString())
+                .putString(USER_IMAGE_URI, newImage.toString())
                 .commit()
             if (isSaved) { finish() }
         }
+
+        user_edit_add_image.setOnClickListener {
+            dispatchTakePictureIntent() }
     }
 
     private fun populateData() {
         user_edit_name.setText(PreferenceManager.getDefaultSharedPreferences(this).getString(USER_NAME, null))
         user_edit_phone_number.setText(PreferenceManager.getDefaultSharedPreferences(this).getString(
             USER_PHONE_NUMBER, null))
+        val imageString = PreferenceManager.getDefaultSharedPreferences(this).getString(USER_IMAGE_URI, null)
+        imageString?.let {
+            val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, Uri.parse(imageString))
+            Glide.with(this).load(bitmap).apply(RequestOptions.circleCropTransform()).into(user_edit_add_image)
+        }
     }
 
     private fun fieldsAreValid(): Boolean {
         return !user_edit_name.text?.trim().isNullOrEmpty() && !user_edit_phone_number.text?.trim().isNullOrEmpty()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == TAKE_IMAGE_REQUEST && resultCode == RESULT_OK) {
+            newImage = data?.data
+            val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, newImage)
+            Glide.with(this).load(bitmap).apply(RequestOptions.circleCropTransform()).into(user_edit_add_image)
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, TAKE_IMAGE_REQUEST)
+            }
+        }
+    }
+
+    private fun checkForPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE)
+            } else {
+            populateData()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    populateData()
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    finish()
+                    Snackbar.make(findViewById(android.R.id.content), "You need to grant the permission to read your profile picture",
+                        Snackbar.LENGTH_SHORT).show()
+                }
+                return
+            }
+
+            // Add other 'when' lines to check for other
+            // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
+            }
+        }
     }
 }
--- app/src/main/java/com/artamonov/look4/WelcomeActivity.kt
+++ app/src/main/java/com/artamonov/look4/WelcomeActivity.kt
@@ -1,14 +1,22 @@
 package com.artamonov.look4
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceManager
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_welcome.*
 
 const val USER_NAME = "USER_NAME"
 const val USER_PHONE_NUMBER = "USER_PHONE_NUMBER"
+const val USER_IMAGE_URI = "USER_IMAGE_URI"
+const val TAKE_IMAGE_REQUEST = 503
+const val PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 5545
+var selectedImage: Uri? = null
 
 class WelcomeActivity : AppCompatActivity() {
 
@@ -23,6 +31,7 @@ class WelcomeActivity : AppCompatActivity() {
                 val isSaved = editor
                     .putString(USER_NAME, etName.text.toString())
                     .putString(USER_PHONE_NUMBER, etPhoneNumber.text.toString())
+                    .putString(USER_IMAGE_URI, selectedImage.toString())
                     .commit()
                 if (isSaved) { startMainActivity() }
             } else {
@@ -30,8 +39,28 @@ class WelcomeActivity : AppCompatActivity() {
             }
         }
 
+        welcome_add_image.setOnClickListener {
+            dispatchTakePictureIntent() }
+
         if (userExists()) {
             startMainActivity()
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, TAKE_IMAGE_REQUEST)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == TAKE_IMAGE_REQUEST && resultCode == RESULT_OK) {
+            selectedImage = data?.data
+            val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, selectedImage)
+            Glide.with(this).load(bitmap).apply(RequestOptions.circleCropTransform()).into(welcome_add_image)
         }
     }
 
@@ -42,10 +71,11 @@ class WelcomeActivity : AppCompatActivity() {
 
     private fun userExists(): Boolean {
         return !PreferenceManager.getDefaultSharedPreferences(this).getString(USER_NAME, null).isNullOrEmpty() &&
-                !PreferenceManager.getDefaultSharedPreferences(this).getString(USER_PHONE_NUMBER, null).isNullOrEmpty()
+                !PreferenceManager.getDefaultSharedPreferences(this).getString(USER_PHONE_NUMBER, null).isNullOrEmpty() &&
+                !PreferenceManager.getDefaultSharedPreferences(this).getString(USER_IMAGE_URI, null).isNullOrEmpty()
     }
 
     private fun fieldsAreValid(): Boolean {
-        return !etName.text?.trim().isNullOrEmpty() && !etPhoneNumber.text?.trim().isNullOrEmpty()
+        return !etName.text?.trim().isNullOrEmpty() && !etPhoneNumber.text?.trim().isNullOrEmpty() && selectedImage != null
     }
 }
